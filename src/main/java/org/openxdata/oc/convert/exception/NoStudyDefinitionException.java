2260b1f3cc521aa78fd89da1d05687c562741b89
Morten Bendiksen <morten.bendiksen@gmail.com>
2011-08-31 18:23:24 +0200

    Fixed issue #1

---
+++ src/main/java/org/openxdata/oc/convert/exception/NoStudyDefinitionException.java
@@ -0,0 +1,9 @@
+package org.openxdata.oc.convert.exception;
+
+@SuppressWarnings("serial")
+public class NoStudyDefinitionException extends Exception {
+
+	public NoStudyDefinitionException(String xml) {
+		super(xml);
+	}
+}
