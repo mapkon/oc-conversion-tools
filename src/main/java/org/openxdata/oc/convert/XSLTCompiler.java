57de37d9fa871b52d50f067f5ce6720e4f7708bc
Morten Bendiksen <morten.bendiksen@gmail.com>
2011-09-19 16:25:36 +0200

    Removed unused code and refactored remaining class.

--- src/main/java/org/openxdata/oc/convert/XSLTCompiler.java
+++ src/main/java/org/openxdata/oc/convert/XSLTCompiler.java
@@ -1,7 +1,9 @@
 package org.openxdata.oc.convert;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 import javax.xml.transform.ErrorListener;
 import javax.xml.transform.TransformerException;
@@ -11,23 +13,29 @@ import net.sf.saxon.s9api.DOMDestination;
 import net.sf.saxon.s9api.Destination;
 import net.sf.saxon.s9api.Processor;
 import net.sf.saxon.s9api.SaxonApiException;
-import net.sf.saxon.s9api.Serializer;
 import net.sf.saxon.s9api.XsltCompiler;
 import net.sf.saxon.s9api.XsltExecutable;
 import net.sf.saxon.s9api.XsltTransformer;
 
 import org.openxdata.oc.convert.exception.InvalidXMLException;
-import org.openxdata.oc.convert.util.XMLUtil;
+import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import com.sun.org.apache.xerces.internal.dom.DOMImplementationImpl;
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 import com.sun.org.apache.xml.internal.security.transforms.TransformationException;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 public class XSLTCompiler implements ErrorListener {
 
+	private Processor processor;
+	private XsltCompiler compiler;
+	
 	private XsltExecutable executable;
-	Processor processor;
-	XsltCompiler compiler;
+	private CompilerUtils utils = new CompilerUtils();
 
 	public XSLTCompiler(String xslt) throws InvalidXMLException {
 		processor = new Processor(false);
@@ -35,7 +43,7 @@ public class XSLTCompiler implements ErrorListener {
 		compiler.setErrorListener(this);
 
 		try {
-			Document xsltDoc = XMLUtil.getDocument(xslt);
+			Document xsltDoc = utils.getDocument(xslt);
 			executable = compiler.compile(new DOMSource(xsltDoc));
 
 		} catch (SaxonApiException e) {
@@ -45,27 +53,28 @@ public class XSLTCompiler implements ErrorListener {
 		}
 	}
 
-	public Document transform(String input) throws InvalidXMLException, TransformationException {
+	public String transform(String input) throws InvalidXMLException,
+			TransformationException {
 		try {
 			XsltTransformer transformer = executable.load();
 			transformer.setErrorListener(this);
-			Document outputDocument = XMLUtil.createDocument();
-			Document inputDocument = XMLUtil.getDocument(input);
+			Document outputDocument = utils.createDocument();
+			Document inputDocument = utils.getDocument(input);
 			inputDocument.getFirstChild();
 			Destination destination = new DOMDestination(outputDocument);
 			transformer.setDestination(destination);
 			DOMSource inputSource = new DOMSource(inputDocument);
 			transformer.setSource(inputSource);
-		
+
 			transformer.transform();
 
-			return outputDocument;
+			return utils.format(outputDocument);
 		} catch (SAXException e) {
 			throw new InvalidXMLException("The intput file is not valid XML", e);
 		} catch (SaxonApiException e) {
-			throw new TransformationException("A transformation error occured: likely a mismatch between xslt and input xml", e);
+			throw new TransformationException(
+					"A transformation error occured: likely a mismatch between xslt and input xml", e);
 		}
-		
 	}
 
 	@Override
@@ -84,5 +93,51 @@ public class XSLTCompiler implements ErrorListener {
 	public void fatalError(TransformerException exception)
 			throws TransformerException {
 		System.err.println(exception);
+	}
+	
+	private class CompilerUtils{
+		
+		Document createDocument(){
+			
+			DOMImplementation impl = DOMImplementationImpl.getDOMImplementation();
+			Document doc = impl.createDocument(null, null, null);
+
+			return doc;
+		}
+		
+		Document getDocument(String xml) throws SAXException {
+			InputSource input = new InputSource(new StringReader(xml));
+			return getDocument(input);
+		}
+
+		Document getDocument(InputSource input) throws SAXException {
+			DOMParser parser = new DOMParser();
+			try {
+				parser.parse(input);
+			} catch (IOException e) {
+				
+				// Since we reading a string, IOException is highly unlikely.
+				throw new RuntimeException("An unexpected IOException occurred.");
+			}
+			
+			Document doc = parser.getDocument();
+			return doc;
+		}
+		
+		String format(Document document) {
+			try {
+				OutputFormat format = new OutputFormat(document);
+				format.setLineWidth(65);
+				format.setIndenting(true);
+				format.setIndent(2);
+				Writer out = new StringWriter();
+				XMLSerializer serializer = new XMLSerializer(out, format);
+				serializer.serialize(document);
+
+				return out.toString();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 }
--- src/main/java/org/openxdata/oc/convert/exception/NoStudyDefinitionException.java
+++
@@ -1,9 +0,0 @@
-package org.openxdata.oc.convert.exception;
-
-@SuppressWarnings("serial")
-public class NoStudyDefinitionException extends Exception {
-
-	public NoStudyDefinitionException(String xml) {
-		super(xml);
-	}
-}
