397c2b24e19c1104436ddd4b9aaafb4989fa8af5
Morten Bendiksen <morten.bendiksen@gmail.com>
2011-09-25 11:12:49 +0200

    Removed extra <ClinicalData/> when building odm file. It is not supposed to be there when importing to openclinica.

--- src/main/groovy/org/openxdata/oc/ODMBuilder.groovy
+++ src/main/groovy/org/openxdata/oc/ODMBuilder.groovy
@@ -1,18 +1,29 @@
 package org.openxdata.oc
 
-import groovy.xml.XmlUtil;
+import groovy.xml.XmlUtil
+
+import java.util.Collection
 
 class ODMBuilder {
 
 	String buildODM(Collection<String> instanceData) {
-		def odm = """<ODM><ClinicalData/></ODM>"""
+		def odm = """<ODM></ODM>"""
 		def parser = new XmlParser()
 		def odmXml = parser.parseText(odm)
 		instanceData.each {
 			def instanceXml = parser.parseText(it)
-			odmXml.ClinicalData.find { true }.children().add(instanceXml)
+			addSubjectData(odmXml, instanceXml)
 		}
-
 		return XmlUtil.serialize(odmXml)
+	}
+
+	def addSubjectData(def xml, def instanceNode) {
+		def studyOID = instanceNode.ClinicalData.@StudyOID[0]
+		def metadataVersion = instanceNode.ClinicalData.@MetaDataVersionOID[0]
+		def clinicalData = xml.ClinicalData.find {it.@StudyOID == studyOID && it.@MetaDataVersionOID == metadataVersion}
+		if (clinicalData == null) {
+			clinicalData = new Node(xml, "ClinicalData", ['StudyOID':studyOID, 'MetaDataVersion':metadataVersion])
+		}
+		clinicalData.append(instanceNode.ClinicalData.SubjectData)
 	}
 }
--- src/test/groovy/org/openxdata/oc/ODMBuilderTest.groovy
+++ src/test/groovy/org/openxdata/oc/ODMBuilderTest.groovy
@@ -1,6 +1,7 @@
 package org.openxdata.oc;
 
 import static org.junit.Assert.*
+import groovy.xml.XmlUtil;
 
 import org.junit.Test
 
@@ -11,50 +12,35 @@ class ODMBuilderTest {
 		
 		def instanceData = new ArrayList<String>()
 		instanceData.add("""<?xml version="1.0" encoding="UTF-8"?>
-						<default_study_-_uganda_se_sc1_sc1-v1 xmlns="" Description="converted from ODM to Xform" formKey="default_study_-_uganda_se_sc1_sc1-v1" id="8" name="SE_SC1_SC1-v1">
-						  <ClinicalData xmlns="http://www.w3.org/2002/xforms" MetaDataVersionOID="v1.0.0" StudyOID="S_12175">
-						    <SubjectData SubjectKey="Mark">
-						      <StudyEventData StudyEventOID="SE_SC1">
-						        <FormData FormOID="F_MSA1_3">
-						          <ItemGroupData ItemGroupOID="IG_MSA1_UNGROUPED">
-						            <ItemData ItemOID="I_MSA1_INIT" value="" Value="Mark Gerard"/>
-						            <ItemData ItemOID="I_MSA1_FORMD" value="" Value="2011-08-23"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_CONS" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_PGT" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_BIRTHD" value="" Value="2011-09-29"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_OLD" value="" Value="26"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_KHIV" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_HAART" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_ELHAART" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_BEFPEP" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_OTRIAL" value="" Value="0"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_OTRIAL_S" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_MOVE" value="" Value="0"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_PMTCT" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_COMPLY" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_ELIG" value="" Value="0"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_DELIVERY" value="" Value="Morten's home"/>
-						            <ItemData ItemOID="I_MSA1_MSA1_CONSELLING" value="" Value="1"/>
-						            <ItemData ItemOID="I_MSA1_IDPAT" value="" Value="0001"/>
-						            <ItemData ItemOID="I_MSA1_ID" value="" Value="001"/>
-						          </ItemGroupData>
-						        </FormData>
-						        <FormData FormOID="F_MSA1_2">
-						          <ItemGroupData ItemGroupOID="IG_MSA1_MSA1_COMPLYREASG">
-						            <ItemData ItemOID="I_MSA1_MSA1_COMPLYREAS" value="" Value="Ask your mother"/>
-						          </ItemGroupData>
-						        </FormData>
-						      </StudyEventData>
-						    </SubjectData>
-						  </ClinicalData>
-						</default_study_-_uganda_se_sc1_sc1-v1>""")
+							<test_study_se_visit_visit-v1 xmlns="" Description="converted from ODM to Xform" formKey="test_study_se_visit_visit-v1" id="10" name="SE_VISIT_Visit-v1">
+							  <ClinicalData xmlns="http://www.w3.org/2002/xforms" MetaDataVersionOID="v1.0.0" StudyOID="S_001">
+							    <SubjectData SubjectKey="SS_MARK">
+							      <StudyEventData StudyEventOID="SE_VISIT">
+							        <FormData FormOID="F_SAMPLECRF_1">
+							          <ItemGroupData ItemGroupOID="IG_SAMPL_UNGROUPED">
+							            <ItemData ItemOID="I_SAMPL_SC_ITEM_01" Value="really" value=""/>
+							            <ItemData ItemOID="I_SAMPL_SC_ITEM_02" Value="ok" value=""/>
+							          </ItemGroupData>
+							          <ItemGroupData ItemGroupOID="IG_SAMPL_GROUP01">
+							            <ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_01" Value="2011-09-15" value=""/>
+							            <ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_02" Value="222" value=""/>
+							          </ItemGroupData>
+							        </FormData>
+							      </StudyEventData>
+							    </SubjectData>
+							  </ClinicalData>
+							</test_study_se_visit_visit-v1>
+							""")
 		
 		def builder = new ODMBuilder()
 		def odm = builder.buildODM(instanceData)
 		
 		def xml = new XmlParser().parseText(odm);
+		
+		println XmlUtil.asString(xml)
+		
 		assertTrue(xml.depthFirst().ItemData.size() > 1)
-		assertEquals(xml.depthFirst().ItemData.size(), 21)
+		assertEquals(xml.depthFirst().ItemData.size(), 4)
 		
 	}
 }
