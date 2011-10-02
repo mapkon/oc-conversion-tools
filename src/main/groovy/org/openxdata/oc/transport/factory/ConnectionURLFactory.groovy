94e108a1c92fd02ff2a594520edefc64aacd182c
kata mapkon <cattabanks@gmail.com>
2011-10-02 14:17:55 +0200

    Refactored ConnectionURLFactory to take host as argument and build url. Closes gh-8.

--- src/main/groovy/org/openxdata/oc/transport/factory/ConnectionURLFactory.groovy
+++ src/main/groovy/org/openxdata/oc/transport/factory/ConnectionURLFactory.groovy
@@ -2,13 +2,13 @@ package org.openxdata.oc.transport.factory
 
 public class ConnectionURLFactory {
 
-	public HttpURLConnection getStudyConnection(){
-		URL url = new URL("http://localhost:8080/OpenClinica-ws-3.1.1/ws/study/v1")
+	public HttpURLConnection getStudyConnection(String host){
+		URL url = new URL(host + "/ws/study/v1")
 		return url.openConnection()
 	}
 
-	public HttpURLConnection getStudySubjectConnectionURL(){
-		URL url = new URL("http://localhost:8080/OpenClinica-ws-3.1.1/ws/studySubject/v1")
+	public HttpURLConnection getStudySubjectConnectionURL(String host){
+		URL url = new URL(host + "/ws/studySubject/v1")
 		return url.openConnection()
 	}
 }
