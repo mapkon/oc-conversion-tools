cb366760e951ca855c8f5b7e8da48f87fbc0ed34
Mark Gerard <cattabanks@gmail.com>
2011-10-03 18:33:52 +0200

    Added Javadoc for ConnectionURLFactory.

--- src/main/groovy/org/openxdata/oc/transport/factory/ConnectionURLFactory.groovy
+++ src/main/groovy/org/openxdata/oc/transport/factory/ConnectionURLFactory.groovy
@@ -1,17 +1,34 @@
 package org.openxdata.oc.transport.factory
 
+/**
+ * Builds a connection for accessing openclinca web services.
+ *
+ */
 public class ConnectionURLFactory {
 
 	def host
+	
+	/**
+	 * Constructs valid endpoints to openclinica web services given a host name.
+	 * @param host Host name to connect to.
+	 */
 	public ConnectionURLFactory(String host){
 		this.host = host
 	}
 	
+	/**
+	 * Gets the connection for accessing the study endpoint in openclinica web services.
+	 * @return URL for connecting to the study endpoint.
+	 */
 	public HttpURLConnection getStudyConnection(){
 		URL url = new URL(host + "/ws/study/v1")
 		return url.openConnection()
 	}
 
+	/**
+	* Gets the connection for accessing the study subject endpoint in openclinica web services.
+	* @return URL for connecting to the study subject endpoint.
+	*/
 	public HttpURLConnection getStudySubjectConnectionURL(){
 		URL url = new URL(host + "/ws/studySubject/v1")
 		return url.openConnection()
