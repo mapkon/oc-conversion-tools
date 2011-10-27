ae356b7463aa9b4eeb663388ab490367fa955521
kata mapkon <cattabanks@gmail.com>
2011-10-27 18:09:53 +0200

    Added files to encapsulate the StudyDef definition. Makes testing easier.

---
+++ src/main/groovy/org/openxdata/oc/model/ConvertedStudyDef.groovy
@@ -0,0 +1,54 @@
+package org.openxdata.oc.model
+
+class ConvertedStudyDef {
+	
+	def id
+	def name
+	def description
+	
+	def forms
+	def bindings
+	
+	def parsedXml
+	def slurper = new XmlSlurper()
+	
+	public def initializeProperties(def xmlStream){
+		this.parsedXml = slurper.parseText(xmlStream)
+		
+		initId()
+		initName()
+		initDescription()
+		
+		initForms()
+		initBindings()
+	}
+	
+	def initId(){
+		this.id = parsedXml.@studyKey.text()
+	}
+	
+	def initName(){
+		this.name = parsedXml.@name.text()
+	}
+	
+	def initDescription(){
+		this.description = parsedXml.@description.text()
+	}
+	
+	def initForms(){
+		this.forms = parsedXml.children()
+	}
+	
+	def initBindings(){
+		def xforms = parsedXml.form.version.xform.text()
+		def xformsText = xforms.replaceFirst('''<?xml version="1.0" encoding="UTF-8" standalone="no"?>''', "")
+		println xformsText
+		def xformNode = new XmlSlurper().parseText(xformsText).declareNamespace(ns:'http://www.w3.org/2002/xforms')
+		
+		this.bindings = parsedXml.form.version.xform.collect{it}
+	}
+	
+	public def getFormVersion(def form){
+		return form.children()
+	}
+}
---
+++ src/test/groovy/org/openxdata/oc/model/ConvertedStudyDefTest.groovy
@@ -0,0 +1,103 @@
+package org.openxdata.oc.model
+
+import groovy.xml.XmlUtil;
+
+import org.junit.Test
+import org.openxdata.oc.Transform
+import org.openxdata.oc.TransformUtil
+
+
+class ConvertedStudyDefTest extends GroovyTestCase {
+
+	def odmDef
+	def odmXmlStream
+
+	def convertedXform
+	def convertedStudyDef
+
+	public void setUp(){
+
+		odmXmlStream = new TransformUtil().loadFileContents("test-odm.xml")
+
+		odmDef = new ODMDefinition()
+		odmDef.initializeProperties(odmXmlStream)
+
+		convertedStudyDef = new ConvertedStudyDef()
+		convertedXform = Transform.getTransformer().transformODM(odmXmlStream, [:])
+
+		convertedStudyDef.initializeProperties(convertedXform)
+	}
+
+	@Test void testInitializeProperties(){
+		assertNotNull convertedStudyDef.id
+		assertNotNull convertedStudyDef.name
+		assertNotNull convertedStudyDef.description
+		assertNotNull convertedStudyDef.forms
+		assertNotNull convertedStudyDef.bindings
+	}
+
+	@Test void testStudyIDMUSTMatchOIDOfODMFile(){
+
+		assertNotNull convertedStudyDef.id
+		assertTrue convertedStudyDef.id.equals(odmDef.OID.toString())
+	}
+
+	@Test void testStudyNameMUSTMatchODMStudyName(){
+
+		def actual = 'Default Study - Uganda'
+
+		assertEquals actual, odmDef.name.toString()
+		assertEquals actual, convertedStudyDef.name
+	}
+
+	@Test void testStudyDescriptionMUSTMatchODMStudyDescription(){
+
+		def actual = 'test instance - Site of Uganda'
+
+		assertEquals actual, odmDef.description.toString()
+		assertEquals actual, convertedStudyDef.description
+	}
+
+	@Test void testStudyShouldHaveCorrectNumberOfForms(){
+
+		def actual1 = 'SE_SC1'
+		def actual2 = 'SE_SC2'
+
+		assertEquals 2, convertedStudyDef.forms.size()
+
+		assertEquals actual1, convertedStudyDef.forms[0].@name.text()
+		assertEquals 'SC1', convertedStudyDef.forms[0].@description.text()
+
+		assertEquals actual2, convertedStudyDef.forms[1].@name.text()
+		assertEquals 'SC2', convertedStudyDef.forms[1].@description.text()
+
+		assertEquals 2, odmDef.studyEventDefs.size()
+	}
+
+	@Test void testStudyShouldHaveCorrectNumberOfFormsGivenStudyEventDefs(){
+
+		assertEquals odmDef.studyEventDefs.size(), convertedStudyDef.forms.size()
+
+		assertEquals odmDef.studyEventDefs[0].@OID.text(), convertedStudyDef.forms[0].@name.text()
+		assertEquals odmDef.studyEventDefs[0].@Name.text(), convertedStudyDef.forms[0].@description.text()
+
+		assertEquals odmDef.studyEventDefs[1].@OID.text(), convertedStudyDef.forms[1].@name.text()
+		assertEquals odmDef.studyEventDefs[1].@Name.text(), convertedStudyDef.forms[1].@description.text()
+	}
+	
+	@Test void testStudyFormMUSTHaveVersion(){
+		
+		def form = convertedStudyDef.forms[0]
+		def version = convertedStudyDef.getFormVersion(form)
+		
+		assertNotNull version
+		assertTrue version.@name.text().contains('-v1')
+		assertEquals form.@description.text()+'-v1', version.@name.text()
+	}
+	
+	@Test void testStudyFormVersionShouldHaveBindings(){
+		
+		println convertedXform
+		assertEquals 45, convertedStudyDef.bindings.size()
+	}
+}
