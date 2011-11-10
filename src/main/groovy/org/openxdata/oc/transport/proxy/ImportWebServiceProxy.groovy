1482264ee97ce3545ba179759117c55ba11d56fe
kata mapkon <cattabanks@gmail.com>
2011-11-10 11:21:10 +0100

    Import web service now uses new error handling mechanism.

--- src/main/groovy/org/openxdata/oc/transport/proxy/ImportWebServiceProxy.groovy
+++ src/main/groovy/org/openxdata/oc/transport/proxy/ImportWebServiceProxy.groovy
@@ -2,6 +2,7 @@ package org.openxdata.oc.transport.proxy
 
 import groovy.util.logging.Log
 
+import org.openxdata.oc.exception.ErrorCode
 import org.openxdata.oc.exception.ImportException
 import org.openxdata.oc.model.ODMDefinition
 import org.openxdata.oc.transport.HttpTransportHandler
@@ -43,7 +44,7 @@ class ImportWebServiceProxy extends SoapRequestProperties {
 		else{
 			
 			log.info("Data Export to OpenClinica Failed with Error: ${result}")
-			throw new ImportException(result)
+			throw new ImportException(ErrorCode.IMPORT_ERROR)
 		}
 	}
 
