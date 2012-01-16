9d1ab6cf491d8c3d4bfee384a72ea1c8e46d9cfa
Kata Mapkon <cattabanks@gmail.com>
2012-01-16 17:22:00 +0300

    Added more tests.

--- src/test/groovy/org/openxdata/oc/transport/proxy/CRFMetaDataVersionProxyTest.groovy
+++ src/test/groovy/org/openxdata/oc/transport/proxy/CRFMetaDataVersionProxyTest.groovy
@@ -210,6 +210,378 @@ class CRFMetaDataVersionProxyTest extends GroovyTestCase {
 		}
 	}
 	
+	@Test void testFindCRFSReturnsResponseWithStudyEventDefHavingFormRef() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[0].children()
+
+			assertNotNull formRefs
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithFirstStudyEventDefHavingONEFormRef() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[0].children()[0]
+
+			assertEquals 1, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithStudyEventDefHavingFormRefWithFormOID_F_MBA2_1() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[0].children()[0]
+
+			assertEquals 'F_MBA2_1', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSECONDStudyEventDefHavingFormRefs() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[1].children()
+
+			assertNotNull formRefs
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSECONDStudyEventDefHavingTWOFormRefs() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[1].children()
+
+			assertEquals 2, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSECONDStudyEventDefHavingFormRefWithFormOID_F_CSAC_3() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[1].children()[0]
+
+			assertEquals 'F_CSAC_3', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSECONDStudyEventDefHavingFormRefWithFormOID_F_MSA2_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[1].children()[1]
+
+			assertEquals 'F_MSA2_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithTHIRDStudyEventDefHavingTWOFormRefs() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[2].children()
+
+			assertEquals 2, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithTHIRDStudyEventDefHavingFormRefWithFormOID_F_SAES_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[2].children()[0]
+
+			assertEquals 'F_SAES_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithTHIRDStudyEventDefHavingFormRefWithFormOID_F_AEAD_3() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[2].children()[1]
+
+			assertEquals 'F_AEAD_3', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithFOURTHStudyEventDefHavingONEFormRef() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[3].children()
+
+			assertEquals 1, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithFOURTHStudyEventDefHavingFormRefWithFormOID_F_SAES_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[3].children()[0]
+
+			assertEquals 'F_MSA1_3', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithFIFTHStudyEventDefHavingONEFormRef() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[4].children()
+
+			assertEquals 1, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithFIFTHStudyEventDefHavingFormRefWithFormOID_F_CILC_1() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[4].children()[0]
+
+			assertEquals 'F_CILC_1', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSIXTHStudyEventDefHavingTWOFormRefs() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[5].children()
+
+			assertEquals 3, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSIXTHStudyEventDefHavingFormRefWithFormOID_F_MEAM_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[5].children()[0]
+
+			assertEquals 'F_MEAM_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSIXTHStudyEventDefHavingFormRefWithFormOID_F_CEAC_4() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[5].children()[1]
+
+			assertEquals 'F_CEAC_4', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSIXTHStudyEventDefHavingFormRefWithFormOID_F_CSDI_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[5].children()[2]
+
+			assertEquals 'F_CSDI_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSEVENTHStudyEventDefHavingONEFormRef() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[6].children()
+
+			assertEquals 1, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithSEVENTHStudyEventDefHavingFormRefWithFormOID_F_CFSC_1() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[6].children()[0]
+
+			assertEquals 'F_CFSC_1', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGHTHStudyEventDefHavingTENFormRefs() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()
+
+			assertEquals 10, formRefs.size()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_CCAC_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[0]
+
+			assertEquals 'F_CCAC_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_CCMC_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[1]
+
+			assertEquals 'F_CCMC_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_CMIC_1() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[2]
+
+			assertEquals 'F_CMIC_1', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_MLLM_5() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[3]
+
+			assertEquals 'F_MLLM_5', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_MHIV_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[4]
+
+			assertEquals 'F_MHIV_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_CLLC_4() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[5]
+
+			assertEquals 'F_CLLC_4', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_CBLC_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[6]
+
+			assertEquals 'F_CBLC_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_CSDC_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[7]
+
+			assertEquals 'F_CSDC_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_MMLM_2() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[8]
+
+			assertEquals 'F_MMLM_2', formRefs.@FormOID.text()
+		}
+	}
+	
+	@Test void testFindCRFSReturnsResponseWithEIGTHStudyEventDefHavingFormRefWithFormOID_F_MBA1_1() {
+		play{
+
+			def response = crfMetaDataVersionProxy.findAllCRFS('OID')
+			def xml = new XmlSlurper().parseText(response)
+
+			def formRefs = xml.Study.MetaDataVersion.StudyEventDef[7].children()[9]
+
+			assertEquals 'F_MBA1_1', formRefs.@FormOID.text()
+		}
+	}
+	
 	private def setUpConnectionFactoryMock() {
 
 		def connection = mock(HttpURLConnection.class)
