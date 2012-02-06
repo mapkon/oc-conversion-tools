e3cb90fdc11ab25e178750f3186a070444d7fe96
Kata Mapkon <cattabanks@gmail.com>
2012-02-06 11:46:27 +0300

    Re factored test to use TestData class.

--- src/test/groovy/org/openxdata/oc/model/ODMDefinitionTest.groovy
+++ src/test/groovy/org/openxdata/oc/model/ODMDefinitionTest.groovy
@@ -1,6 +1,7 @@
 package org.openxdata.oc.model
 
 import org.junit.Test
+import org.openxdata.oc.data.TestData
 import org.openxdata.oc.exception.ImportException
 
 
@@ -10,29 +11,9 @@ class ODMDefinitionTest extends GroovyTestCase {
 	def instanceData = new ArrayList<String>()
 	
 	public void setUp(){
-
-		instanceData.add("""<?xml version="1.0" encoding="UTF-8"?>
-							<test_study_se_visit_visit-v1 xmlns="" Description="converted from ODM to Xform" formKey="test_study_se_visit_visit-v1" id="10" name="SE_VISIT_Visit-v1">
-							  <ClinicalData xmlns="http://www.w3.org/2002/xforms" MetaDataVersionOID="v1.0.0" StudyOID="S_001">
-								<SubjectData SubjectKey="SS_MARK">
-								  <StudyEventData StudyEventOID="SE_VISIT">
-									<FormData FormOID="F_SAMPLECRF_1">
-									  <ItemGroupData ItemGroupOID="IG_SAMPL_UNGROUPED">
-										<ItemData ItemOID="I_SAMPL_SC_ITEM_01" Value="really" value=""/>
-										<ItemData ItemOID="I_SAMPL_SC_ITEM_02" Value="ok" value=""/>
-									  </ItemGroupData>
-									  <ItemGroupData ItemGroupOID="IG_SAMPL_GROUP01">
-										<ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_01" Value="2011-09-15" value=""/>
-										<ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_02" Value="222" value=""/>
-									  </ItemGroupData>
-									</FormData>
-								  </StudyEventData>
-								</SubjectData>
-							  </ClinicalData>
-							</test_study_se_visit_visit-v1>""")
 		
 		def odmDef = new ODMDefinition()
-		exportedInstanceData = odmDef.appendInstanceData(instanceData)
+		exportedInstanceData = odmDef.appendInstanceData(TestData.getInstanceData())
 	}
 	
 	@Test void testAppendInstanceDataDoesNotReturnNull() {
@@ -56,4 +37,6 @@ class ODMDefinitionTest extends GroovyTestCase {
 			new ODMDefinition().appendInstanceData(emptyInstanceData)
 		}
 	}
+	
+	
 }
