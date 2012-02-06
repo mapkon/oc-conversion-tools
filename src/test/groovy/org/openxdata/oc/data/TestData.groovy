94677be632bd4b411fce2e0ebff869c31f98d54b
Kata Mapkon <cattabanks@gmail.com>
2012-02-06 12:07:33 +0300

    Formatted this xml.

--- src/test/groovy/org/openxdata/oc/data/TestData.groovy
+++ src/test/groovy/org/openxdata/oc/data/TestData.groovy
@@ -16,25 +16,24 @@ class TestData {
 		
 		def instanceData = []
 		
-		def testInstanceData = """<?xml version="1.0" encoding="UTF-8"?>
-							<test_study_se_visit_visit-v1 xmlns="" Description="converted from ODM to Xform" formKey="test_study_se_visit_visit-v1" id="10" name="SE_VISIT_Visit-v1">
-							  <ClinicalData xmlns="http://www.w3.org/2002/xforms" MetaDataVersionOID="v1.0.0" StudyOID="S_001">
-								<SubjectData SubjectKey="SS_MARK">
-								  <StudyEventData StudyEventOID="SE_VISIT">
-									<FormData FormOID="F_SAMPLECRF_1">
-									  <ItemGroupData ItemGroupOID="IG_SAMPL_UNGROUPED">
-										<ItemData ItemOID="I_SAMPL_SC_ITEM_01" Value="really" value=""/>
-										<ItemData ItemOID="I_SAMPL_SC_ITEM_02" Value="ok" value=""/>
-									  </ItemGroupData>
-									  <ItemGroupData ItemGroupOID="IG_SAMPL_GROUP01">
-										<ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_01" Value="2011-09-15" value=""/>
-										<ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_02" Value="222" value=""/>
-									  </ItemGroupData>
-									</FormData>
-								  </StudyEventData>
-								</SubjectData>
-							  </ClinicalData>
-							</test_study_se_visit_visit-v1>"""
+		def testInstanceData = """<test_study_se_visit_visit-v1 xmlns="" Description="converted from ODM to Xform" formKey="test_study_se_visit_visit-v1" id="10" name="SE_VISIT_Visit-v1">
+								  <ClinicalData xmlns="http://www.w3.org/2002/xforms" MetaDataVersionOID="v1.0.0" StudyOID="S_001">
+									<SubjectData SubjectKey="SS_MARK">
+									  <StudyEventData StudyEventOID="SE_VISIT">
+										<FormData FormOID="F_SAMPLECRF_1">
+										  <ItemGroupData ItemGroupOID="IG_SAMPL_UNGROUPED">
+											<ItemData ItemOID="I_SAMPL_SC_ITEM_01" Value="really" value=""/>
+											<ItemData ItemOID="I_SAMPL_SC_ITEM_02" Value="ok" value=""/>
+										  </ItemGroupData>
+										  <ItemGroupData ItemGroupOID="IG_SAMPL_GROUP01">
+											<ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_01" Value="2011-09-15" value=""/>
+											<ItemData ItemOID="I_SAMPL_SC_REPEATING_ITEM_02" Value="222" value=""/>
+										  </ItemGroupData>
+										</FormData>
+									  </StudyEventData>
+									</SubjectData>
+								  </ClinicalData>
+								</test_study_se_visit_visit-v1>"""
 		
 		instanceData.add(testInstanceData)
 		
