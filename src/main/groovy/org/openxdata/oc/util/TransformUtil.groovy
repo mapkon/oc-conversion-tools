f4276acf6cb821ef65ec2906a7ec64e64043efc5
Kata Mapkon <cattabanks@gmail.com>
2012-02-22 12:30:40 +0300

    Reverting unfinished work.

--- src/main/groovy/org/openxdata/oc/util/TransformUtil.groovy
+++ src/main/groovy/org/openxdata/oc/util/TransformUtil.groovy
@@ -4,7 +4,7 @@ public class TransformUtil {
 
 	public def loadFileContents(def fileName){
 		if(fileName){
-			def stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("${fileName}")
+			def stream = getClass().getResourceAsStream("../${fileName}")
 			return stream.text
 		}else{
 			throw new IllegalArgumentException('File name cannot be null or empty.')
