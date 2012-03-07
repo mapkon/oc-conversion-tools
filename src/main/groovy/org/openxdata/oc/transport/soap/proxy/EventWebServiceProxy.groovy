fea50e4b4228e8ea760dc11334ca9e001c434b01
Kata Mapkon <cattabanks@gmail.com>
2012-03-07 16:32:25 +0300

    Minor clean up.

--- src/main/groovy/org/openxdata/oc/transport/soap/proxy/EventWebServiceProxy.groovy
+++ src/main/groovy/org/openxdata/oc/transport/soap/proxy/EventWebServiceProxy.groovy
@@ -2,7 +2,7 @@ package org.openxdata.oc.transport.soap.proxy
 
 import groovy.util.logging.Log
 
-import org.openxdata.oc.transport.HttpTransportHandler;
+import org.openxdata.oc.transport.HttpTransportHandler
 import org.openxdata.oc.transport.soap.SoapRequestProperties
 
 @Log
