32cb5b5730b1423bd2b6e61fa595333153c1d022
Kata Mapkon <cattabanks@gmail.com>
2012-03-23 20:10:17 +0300

    Formatting sources using the openxdata-formatter. This is to keep the code bases in sync.

--- src/main/java/org/openxdata/oc/servlet/OpenClinicaServlet.java
+++ src/main/java/org/openxdata/oc/servlet/OpenClinicaServlet.java
@@ -36,26 +36,26 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 public class OpenClinicaServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6577932874016086164L;
-		
+
 	private final String IMPORT = "Import";
 	private final String EXPORT = "Export";
 	private final String JSP_LOCATION = "openclinica.jsp";
-	
+
 	private FormService formService;
 	private StudyManagerService studyService;
 	private Properties props = new Properties();
-	
+
 	private DataExportService dataExportService;
 	private OpenClinicaService openclinicaService;
 	private AuthenticationService authenticationService;
-	
+
 	private final Logger log = LoggerFactory.getLogger(OpenClinicaServlet.class);
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 
 		super.init(config);
-		
+
 		ServletContext sctx = this.getServletContext();
 		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(sctx);
 
@@ -63,9 +63,9 @@ public class OpenClinicaServlet extends HttpServlet {
 		studyService = (StudyManagerService) ctx.getBean("studyManagerService");
 		dataExportService = (DataExportService) ctx.getBean("dataExportService");
 		authenticationService = (AuthenticationService) ctx.getBean("authenticationService");
-		
-    	props = loadProperties();
-    	
+
+		props = loadProperties();
+
 		openclinicaService = new OpenClinicaServiceImpl(props);
 
 		openclinicaService.setStudyService(studyService);
@@ -73,169 +73,168 @@ public class OpenClinicaServlet extends HttpServlet {
 		openclinicaService.setDataExportService(dataExportService);
 
 	}
-	
-    private Properties loadProperties() {
-    	
-    	try {
-    		
+
+	private Properties loadProperties() {
+
+		try {
+
 			log.debug("Attempting to load properties file from Web Context");
 			InputStream propFileStream = getServletContext().getResourceAsStream("openclinica.properties");
 			props.load(propFileStream);
-			
+
 		} catch (Exception e) {
-			
+
 			// We assume the absence of an external properties file in openxdata/WEB-INF
 			loadInternalProperties();
 		}
-    	
+
 		return props;
 	}
 
 	private void loadInternalProperties() {
-		
+
 		try {
-			
+
 			log.debug("File not found in Web context. Attempting to load internal properties file");
-			
+
 			String propFileContent = new TransformUtil().loadFileContents("META-INF/openclinica.properties");
 			props.load(new ByteArrayInputStream(propFileContent.getBytes()));
-			
+
 		} catch (IOException e) {
 			log.debug(e.getLocalizedMessage());
 		}
 	}
 
 	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-    	
-    	try {
-    		
-    		HashMap<String, String> map = new HashMap<String, String>();
-    		map.put(" ", "No studies downloaded during this session.");
-    		
-    		request.setAttribute("message", map);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+		try {
+
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put(" ", "No studies downloaded during this session.");
+
+			request.setAttribute("message", map);
 			request.getRequestDispatcher(JSP_LOCATION).forward(request, response);
-			
+
 		} catch (ServletException e) {
 			log.error(e.getLocalizedMessage());
 		} catch (IOException e) {
 			log.error(e.getLocalizedMessage());
 		}
-    }
-	
+	}
+
 	@Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
 		HashMap<String, String> map = new HashMap<String, String>();
 		try {
-			
-	    	StudyDef study = null;
-	    	String oid = request.getParameter("oid");
-	    	String action = request.getParameter("action");
 
-	    	User user = authenticate();
+			StudyDef study = null;
+			String oid = request.getParameter("oid");
+			String action = request.getParameter("action");
 
-	    	if(IMPORT.equals(action)) {
-	    		
-	    		study = fetchAndSaveStudy(oid);
-	    		
-	    		map.put("", "OpenClinica Study successfully converted and Imported");
-	    		
-	    		request.setAttribute("message", map);
-	    		
-	    		request.setAttribute("study", study);
-		    	request.setAttribute("name", study.getName());
-		    	request.setAttribute("key", study.getStudyKey());
-	    	}
-	    	else if(EXPORT.equals(action)) {
-	    		
-	    		HashMap<String, String> messages = exportStudyData();
-	    		request.setAttribute("message", messages);
-	    	}
-	    	
-	    	request.setAttribute("user", user);
-	    	
-	    	request.getRequestDispatcher(JSP_LOCATION).forward(request, response);
-	    	
-		}catch (Exception ex) {
-			
+			User user = authenticate();
+
+			if (IMPORT.equals(action)) {
+
+				study = fetchAndSaveStudy(oid);
+
+				map.put("", "OpenClinica Study successfully converted and Imported");
+
+				request.setAttribute("message", map);
+
+				request.setAttribute("study", study);
+				request.setAttribute("name", study.getName());
+				request.setAttribute("key", study.getStudyKey());
+			} else if (EXPORT.equals(action)) {
+
+				HashMap<String, String> messages = exportStudyData();
+				request.setAttribute("message", messages);
+			}
+
+			request.setAttribute("user", user);
+
+			request.getRequestDispatcher(JSP_LOCATION).forward(request, response);
+
+		} catch (Exception ex) {
+
 			log.error(ex.getLocalizedMessage());
 			map.clear();
 			request.setAttribute("message", map.put("", ex.getLocalizedMessage()));
-			
-	    	redirectToOpenClinicaPage(request, response);
-		}
-    }
 
-	private void redirectToOpenClinicaPage(HttpServletRequest request,HttpServletResponse response) {
+			redirectToOpenClinicaPage(request, response);
+		}
+	}
+
+	private void redirectToOpenClinicaPage(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			
+
 			response.sendRedirect(JSP_LOCATION);
-			
+
 		} catch (IOException ex) {
 			log.error(ex.getLocalizedMessage());
 		}
 	}
-	
+
 	private User authenticate() {
-		
+
 		User user = null;
-		if(authenticationService != null) {
-			
+		if (authenticationService != null) {
+
 			String oxdUserName = props.getProperty("oxdUserName");
 			String oxdPassword = props.getProperty("oxdPassword");
-			
+
 			log.info("Authenticating User: " + oxdUserName);
 
 			user = authenticationService.authenticate(oxdUserName, oxdPassword);
-			if(user == null)
-	    		throw new OpenXdataDataAccessException("Access to OpenXData Services Denied");
-			
+			if (user == null)
+				throw new OpenXdataDataAccessException("Access to OpenXData Services Denied");
+
 		}
 		return user;
 	}
-	
+
 	private HashMap<String, String> exportStudyData() throws ImportException {
-		
+
 		log.info("Initiating Export of Study Data to OpenClinica...");
-		
+
 		HashMap<String, String> exportMessages = openclinicaService.exportOpenClinicaStudyData();
-		if("Error".equals(exportMessages))
+		if ("Error".equals(exportMessages))
 			throw new ImportException("Exception occurred during export of data to openclinica. Check log for details.");
-		
+
 		return exportMessages;
 	}
-    
+
 	private StudyDef fetchAndSaveStudy(String oid) {
-		
+
 		log.info("Fetching study for oid: " + oid);
 		StudyDef study = openclinicaService.importOpenClinicaStudy(oid);
 		study = validateAndSaveStudy(study);
-		
+
 		return study;
 	}
 
 	private StudyDef validateAndSaveStudy(StudyDef study) {
 
 		if (studyService != null) {
-			
+
 			log.info("Validating Converted Study: " + study.getName());
 			StudyDef existingStudy = studyService.getStudyByKey(study.getStudyKey());
-			
+
 			if (existingStudy != null) {
-				
+
 				List<FormDefVersion> existingStudyFormVersions = getStudyFormVersions(existingStudy);
 
 				for (FormDefVersion version : existingStudyFormVersions) {
 					inspectStudyFormVersions(version, study);
 				}
-				
+
 				log.info("Saving existing [converted] Study: " + existingStudy.getName());
 
 				studyService.saveStudy(existingStudy);
 
-			}else {
-				
+			} else {
+
 				log.info("First time download - Saving new converted study: " + study.getName());
 
 				studyService.saveStudy(study);
@@ -256,32 +255,32 @@ public class OpenClinicaServlet extends HttpServlet {
 			}
 		}
 	}
-	
+
 	private List<FormDefVersion> getStudyFormVersions(StudyDef study) {
 		List<FormDefVersion> versions = new ArrayList<FormDefVersion>();
-		for(FormDef form : study.getForms()) {
+		for (FormDef form : study.getForms()) {
 			versions.add(form.getDefaultVersion());
 		}
 		return versions;
 	}
 
 	private void incrementAndMakeDefault(FormDefVersion versionToIncrement, FormDef form) {
-		
+
 		log.info("Incrementing to latest Version: " + versionToIncrement.getName());
 
 		String incrementVersionName = versionToIncrement.getName();
 		String nextVersion = form.getNextVersionName();
-		
-		String newVersionName = incrementVersionName.
-				replace(incrementVersionName.substring(incrementVersionName.length() -  2), nextVersion);
-		
+
+		String newVersionName = incrementVersionName.replace(
+				incrementVersionName.substring(incrementVersionName.length() - 2), nextVersion);
+
 		versionToIncrement.setName(newVersionName);
-		
+
 		versionToIncrement.setFormDef(form);
 		form.addVersion(versionToIncrement);
-		
+
 		form.turnOffOtherDefaults(versionToIncrement);
 		form.setDirty(true);
-		
+
 	}
 }
