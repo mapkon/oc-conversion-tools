9e2006a84c016f5c63c194ef4d63c68abac8d616
Your Name <noryak1234@gmail.com>
2012-04-16 13:57:15 +0400

    Removed Tests from the java package to groovy

---
+++ src/test/groovy/org/openxdata/oc/Fixtures.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openxdata.oc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.openxdata.server.admin.model.Editable;
+import org.openxdata.server.admin.model.StudyDef;
+import org.openxdata.oc.model.StudySubject;
+
+/**
+ *
+ * @author kay
+ */
+public class Fixtures {
+
+	public static StudyDef getOXDStudy() {
+		try {
+			InputStream resourceAsStream = Fixtures.class.getClassLoader().getResourceAsStream("Default_Study.xml");
+			String toString = IOUtils.toString(resourceAsStream);
+			Editable importStudyItem = StudyImport.importStudyItem(toString);
+
+			return (StudyDef) importStudyItem;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+	public static List<StudySubject> getStudySubjectEvents() {
+		try {
+			InputStream resourceAsStream = Fixtures.class.getClassLoader().getResourceAsStream(
+					"subject-event-response.xml");
+			String studySubjectResponse = IOUtils.toString(resourceAsStream);
+			return null;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+}
