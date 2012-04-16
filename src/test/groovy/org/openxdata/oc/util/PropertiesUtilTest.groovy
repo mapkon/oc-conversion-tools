2ba54239a7e7817c25566e0caba29591699b1323
Your Name <noryak1234@gmail.com>
2012-04-16 17:25:10 +0400

    Fixed failing test

--- src/test/groovy/org/openxdata/oc/util/PropertiesUtilTest.groovy
+++ src/test/groovy/org/openxdata/oc/util/PropertiesUtilTest.groovy
@@ -44,7 +44,7 @@ class PropertiesUtilTest extends GroovyTestCase {
 	@Test void testLoadPropertiesReturnsValidHostProperty() {
 
 		def host = props.getAt('host')
-		assertEquals 'http://10.10.3.217:8080/OpenClinica-ws-SNAPSHOT', host
+		assertEquals 'http://158.37.6.165/OpenClinica-ws-SNAPSHOT', host
 	}
 
 	@Test void testLoadPropertiesReturnsUserNameProperty() {
@@ -78,7 +78,7 @@ class PropertiesUtilTest extends GroovyTestCase {
 
 	@Test void testGetOCPropertyReturnsValidHost() {
 		def host = util.getOCProperty('host')
-		assertNotNull 'http://10.10.3.217:8080/OpenClinica-ws-SNAPSHOT', host
+		assertNotNull 'http://158.37.6.165/OpenClinica-ws-SNAPSHOT', host
 	}
 
 	@Test void testGetOCPropertyReturnsUserNameProperty() {
