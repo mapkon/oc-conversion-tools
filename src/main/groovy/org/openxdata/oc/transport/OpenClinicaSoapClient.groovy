d59000e0adcbc679ea686fa1b5e0b73757e50174
Kata Mapkon <cattabanks@gmail.com>
2012-10-08 17:37:53 +0200

    Added some javadocs to public interfaces.

--- src/main/groovy/org/openxdata/oc/transport/OpenClinicaSoapClient.groovy
+++ src/main/groovy/org/openxdata/oc/transport/OpenClinicaSoapClient.groovy
@@ -8,18 +8,58 @@ import org.openxdata.server.admin.model.User
 
 /**
  * Defines methods that represent endpoints OpenClinica Web services. Note that this is not a web service in itself but
- * rather tries to map the methods to resemble the endpoints defined in the web services.
- *
+ * rather tries to map the methods to endpoints defined in OpenClinica web services.
+ * <p>
+ * This should be used in tandem with the application specific services to consume what OpenClinica exposes through the web services.
+ *<p>
+ * The methods in this interface assume that the user who is retrieving the events has access to perform web services operations in OpenClinica.
  */
 public interface OpenClinicaSoapClient {
 
+	/**
+	 * Extracts all CRFs for a given study from OpenClinica
+	 * 
+	 * @param studyOID Identifier for study we are extracting CRFs for.
+	 * 
+	 * @return ODM XML representation of all the CRFs in a given OpenClinica study.
+	 */
 	def findAllCRFS(def studyOID)
 		
+	/**
+	 * Extracts an OpenXData Forms from ODM XML
+	 * 
+	 * @param studyIdentifier Identifier for OpenClinica study we are extracting forms from.
+	 * 
+	 * @return Fully compact OpenXdata Xform XML
+	 */
 	def getOpenxdataForm(def studyIdentifier)
 
+	/**
+	 * Exports the given OpenXdata instance data to OpenClinica.
+	 * <p>
+	 * This method assumes that the instance data has a studyKey corresponding to an existing study in OpenClinica.
+	 * 
+	 * @param user User who submitted the data
+	 * @param instanceData OpenXdata instance data to export to OpenClinica.
+	 * @return Map of FormIds and Strings showing status of each FormData export
+	 */
 	HashMap<String, String> importData(User user, List<FormData> instanceData)
 	
+	/**
+	 * Retrieves all the events for the subjects in a given study that the user has access to. By user we mean the user initiating the web service operation.
+	 * 
+	 * @param studyOID OID of the study we are retrieving events from.
+	 * @return ODM XML of the events.
+	 */
 	List<StudySubject> findStudySubjectEventsByStudyOID(def studyOID)
 	
+	/**
+	 * Retrieves partial details for a given OpenClinica user.
+	 * <p>
+	 * This method assumes that the username exists in OpenClinica.
+	 * 
+	 * @param username User name of the user we retrieving details for.
+	 * @return OpenClinica User with the details.
+	 */
 	OpenClinicaUser getUserDetails(def username)
 }
