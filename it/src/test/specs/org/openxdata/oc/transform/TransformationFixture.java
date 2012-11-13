253e15afc611778e473b315ac3ef6d164247f172
mapkon <cattabanks@gmail.com>
2012-11-13 23:42:00 +0100

    Added integration tests as a different project. Re factored pom files to remove unnecessary config.

---
+++ it/src/test/specs/org/openxdata/oc/transform/TransformationFixture.java
@@ -0,0 +1,47 @@
+package org.openxdata.oc.transform;
+
+import java.util.Properties;
+
+import org.concordion.api.extension.Extensions;
+import org.concordion.ext.TimestampFormatterExtension;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.openxdata.oc.service.OpenClinicaService;
+import org.openxdata.oc.service.impl.OpenClinicaServiceImpl;
+import org.openxdata.oc.util.PropertiesUtil;
+import org.openxdata.server.admin.model.StudyDef;
+
+@RunWith(ConcordionRunner.class)
+@Extensions(TimestampFormatterExtension.class)
+public class TransformationFixture {
+
+	private StudyDef getStudy() {
+
+		Properties props = new PropertiesUtil().loadProperties("META-INF/openclinica.properties");
+		OpenClinicaService openclinicaService = new OpenClinicaServiceImpl(props);
+
+		StudyDef study = openclinicaService.importOpenClinicaStudy("S_DEFAULTS1");
+
+		return study;
+	}
+
+	public String getStudyName() {
+
+		return getStudy().getName();
+	}
+
+	public String getStudyKey() {
+
+		return getStudy().getStudyKey();
+	}
+
+	public int getForms() {
+
+		return getStudy().getForms().size();
+	}
+
+	public String getFormName(String formName) {
+
+		return getStudy().getForm(formName).getName();
+	}
+}
